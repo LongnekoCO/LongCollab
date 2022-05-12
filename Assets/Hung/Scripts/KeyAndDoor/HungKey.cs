791738478c5b8c3232403702bd422f253d3f7052
WarLight235 <lehoanghung2002@gmail.com>
2022-05-12 12:12:18 +0700

    Clearing Script

--- Assets/Hung/Scripts/KeyAndDoor/HungKey.cs
+++ Assets/Hung/Scripts/KeyAndDoor/HungKey.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class HungKey : MonoBehaviour
 {
-    private PlayerMovementScript player;
-    public GameObject door;
-
     [SerializeField] private KeyType keyType;
 
     public enum KeyType
@@ -20,20 +17,12 @@ public class HungKey : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Dog").GetComponent<PlayerMovementScript>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            Destroy(door);
-        }
     }
 }
