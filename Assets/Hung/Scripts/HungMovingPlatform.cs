e5fba813fe28c9ad555431a7089eb760bd5eba3e
EssSohail <97175325+EssSohail@users.noreply.github.com>
2022-05-16 15:59:41 +0700

    I push

--- Assets/Hung/Scripts/HungMovingPlatform.cs
+++ Assets/Hung/Scripts/HungMovingPlatform.cs
@@ -32,6 +32,8 @@ public class HungMovingPlatform : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
+
         if (collision.gameObject.tag == "Waypoint")
         {
             isMovingForward = true;
@@ -43,4 +45,21 @@ public class HungMovingPlatform : MonoBehaviour
             isMovingBackward = true;
         }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.transform.SetParent(transform);
+        }
+        
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.transform.SetParent(null);
+        }
+        //collision.transform.SetParent(null);
+    }
+
 }
