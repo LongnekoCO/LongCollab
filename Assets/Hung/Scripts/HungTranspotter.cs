95d08975db70852d294f90cd95dfd39d077437b1
WarLight235 <92709109+WarLight235@users.noreply.github.com>
2022-05-18 10:52:48 +0700

    Fixing

--- Assets/Hung/Scripts/HungTranspotter.cs
+++ Assets/Hung/Scripts/HungTranspotter.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class HungTranspotter : MonoBehaviour
 {
+    public float speed;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
