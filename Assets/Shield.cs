7bfbbbd53d2a2b6872943af515d0dd2f028f231a
nguyenktran197 <94814285+nguyenktran197@users.noreply.github.com>
2022-05-20 17:01:05 +0700

    Shield Fix

--- Assets/Shield.cs
+++ Assets/Shield.cs
@@ -27,6 +27,7 @@ public class Shield : MonoBehaviour
         rb = this.GetComponent<Rigidbody2D>();
         state = State.Recalling;
         trail = this.GetComponent<TrailRenderer>();
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 1);
     }
 
     void FixedUpdate()
@@ -40,6 +41,7 @@ public class Shield : MonoBehaviour
                 Vector3 dirToPlayer = (player.GetPosition() - transform.position).normalized;
                 float recallSpeed = 50f;
                 rb.velocity = dirToPlayer * recallSpeed;
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 1);
                 //var step = recallSpeed * Time.deltaTime;
                 //transform.position = Vector3.MoveTowards(transform.position, player.GetPosition(), step);
                 TryPlayerGrabShield();
@@ -53,7 +55,8 @@ public class Shield : MonoBehaviour
         switch (state)
         {
             case State.WithPlayer:
-                transform.position = shieldHolder.position;                
+                transform.position = shieldHolder.position;
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 1);
                 break;
         }
     }
@@ -77,6 +80,7 @@ public class Shield : MonoBehaviour
         rb.AddForce(throwDir * throwForce, ForceMode2D.Impulse);
         trail.enabled = true;
         state = State.Thrown;
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 1);
     }
 
     public void Recall()
