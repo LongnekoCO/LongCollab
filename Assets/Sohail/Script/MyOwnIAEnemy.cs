87fa99464da171a0329396d1ba64bdbd9585b8e2
EssSohail <97175325+EssSohail@users.noreply.github.com>
2022-06-01 23:14:14 +0700

    push

---
+++ Assets/Sohail/Script/MyOwnIAEnemy.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int enemyHealth;
+    public int enemyValue;
+    public float aniDelay;
+
+    public int rutine;
+    public float cronometer;
+    public Animator ani;
+    public int direction;
+
+    public float speed_walk;
+    public float speed_run;
+
+    public Transform player;
+
+    public bool isAttack;
+
+    public float vision_range;
+    public float vision_atack;
+
+
+
+
+    /*public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "bullet")
+        {
+            enemyHealth -= Bullet.damage;
+
+            if (enemyHealth <= 0)
+            {
+                ani.SetBool("die", true);
+                Debug.Log(enemyValue);
+                Destroy(gameObject, aniDelay);
+            }
+
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+        player = transform.Find("Player");
+    }
+
+
+    public void behaviors()
+    {
+        //if (Mathf.Abs(transform.position.x - player.transform.position.x) > vision_range && !isAttack)
+        //{
+        ani.SetBool("run", false);
+        cronometer += 1 * Time.deltaTime;
+        if (cronometer >= 4)
+        {
+            rutine = Random.Range(0, 2);
+            cronometer = 0;
+        }
+        switch (rutine)
+        {
+            case 0:
+                ani.SetBool("walk", false);
+                break;
+
+            case 1:
+                direction = Random.Range(0, 2);
+                rutine++;
+                break;
+
+            case 2:
+
+                switch (direction)
+                {
+                    case 0:
+                        transform.rotation = Quaternion.Euler(0, 0, 0);
+                        transform.Translate(Vector3.right * speed_walk * Time.deltaTime);
+                        break;
+
+                    case 1:
+                        transform.rotation = Quaternion.Euler(0, 180, 0);
+                        transform.Translate(Vector3.right * speed_walk * Time.deltaTime);
+                        break;
+                }
+                ani.SetBool("walk", true);
+                break;
+        }
+        //}
+    }
+    //Update is called once per frame
+    void Update()
+    {
+        behaviors();
+    }*/
+
+}
