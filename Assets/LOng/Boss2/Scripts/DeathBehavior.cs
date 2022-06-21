5cf7424bd675aa6505860a3cdb5333b9ab105a99
Longnek0 <hunglong2000101@gmail.com>
2022-06-21 22:13:28 +0700

    Adding in 2 bosses

---
+++ Assets/LOng/Boss2/Scripts/DeathBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBehavior : StateMachineBehaviour {
+
+    private LongBoss2 boss;
+
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        boss = animator.GetComponent<LongBoss2>();
+        boss.isDead = true;
+	}
+
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	
+	}
+
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	
+	}
+
+
+}
