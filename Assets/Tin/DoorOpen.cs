d49f713991c389cb31eeaf47496b09dddf4163ef
Nguyễn Thành Tín <thanhtin276@gmail.com>
2022-06-24 17:27:12 +0700

    Update Choosing Level Screen
    Big Update: No longer using player to open to the door, this time using cursor to open the door

--- Assets/Tin/DoorOpen.cs
+++ Assets/Tin/DoorOpen.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DoorOpen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool enterAllowed;
+    public string sceneToLoad;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.GetComponent<DoorTutorial>())
+        {
+            enterAllowed = true;
+            sceneToLoad = "TutorialScene";
+            Debug.Log("Tutorial");
+
+        }
+        else if (collision.GetComponent<DoorForest>())
+        {
+            enterAllowed = true;
+            sceneToLoad = "ForestScene";
+
+        }
+        else if (collision.GetComponent<DoorIce>())
+        {
+            enterAllowed = true;
+            sceneToLoad = "IceScene";
+
+        }
+        else if (collision.GetComponent<DoorFuture>())
+        {
+            enterAllowed = true;
+            sceneToLoad = "FutureScene";
+
+        }
+        else if (collision.GetComponent<DoorDungeon>())
+        {
+            enterAllowed = true;
+            sceneToLoad = "DungeonScene";
+
+        }
     }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<DoorTutorial>()|| collision.GetComponent<DoorForest>()|| collision.GetComponent<DoorIce>()|| collision.GetComponent<DoorFuture>()|| collision.GetComponent<DoorDungeon>())
+        {
+            enterAllowed = false;
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(enterAllowed && Input.GetKey(KeyCode.F))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
     }
 }
--- Assets/Tin/DoorTutorial.cs
+++ Assets/Tin/DoorTutorial.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class DoorTutorial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+
+
+
+    public void OpenTutorialDoor()
     {
-        
+        StartCoroutine(Tutorial());
+    }
+ 
+    IEnumerator Tutorial()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("TutorialScene");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
