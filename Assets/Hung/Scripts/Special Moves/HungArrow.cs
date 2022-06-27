c68ed07b89ea528fc37ba370095275d9863e86f4
WarLight235 <92709109+WarLight235@users.noreply.github.com>
2022-06-27 23:37:21 +0700

    Adding platform for the forest level

--- Assets/Hung/Scripts/Special Moves/HungArrow.cs
+++ Assets/Hung/Scripts/Special Moves/HungArrow.cs
@@ -21,7 +21,7 @@ public class HungArrow : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
